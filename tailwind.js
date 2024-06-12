972cf7d55c57de14bccd0f56d8f139a72865f2ee
john-karanja-at-433947150609 <john.karanja@dentsu.com>
2024-06-13 01:52:37 +0300

    added about

--- tailwind.js
+++ tailwind.js
@@ -4,10 +4,20 @@
         extend: {
           colors: {
             clifford: '#da373d',
+            'nbk-brown': '#3c251f',
+            'nbk-yellow': '#FFBA00',
           },
           fontFamily: {
             'poppins': ["Poppins", 'sans-serif'],
             'montserrat': ['Montserrat', 'sans-serif'],
+          },
+          gridTemplateColumns: {
+            // Simple 16 column grid
+            'four-columns': 'repeat(auto-fit, minmax(180px, 1fr))',
+            'five-columns': 'repeat(auto-fit, minmax(180px, 1fr))',
+    
+            // Complex site-specific column configuration
+            'footer': '200px minmax(900px, 1fr) 100px',
           }
         }
       }
